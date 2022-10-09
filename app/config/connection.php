ee3cbaf133e93a36d88588b0b867f92a69311778
CESCO <cescovalledev@gmail.com>
2022-10-09 15:55:36 -0600

    Connection empezada

---
+++ app/config/connection.php
@@ -0,0 +1,23 @@
+<?php
+class Connection
+{
+    public $host = 'localhost';
+    public $dbname = 'dbname';
+    public $port = '5432';
+    public $username = 'root';
+    public $password = 'root';
+    public $driver = 'pgsql';
+    public $connect;
+
+    public static function getConnection()
+    {
+        try {
+            $connection = new Connection();
+            $connection->connect = new PDO("{$connection->driver}:host={$connection->host}; port={$connection->port};dbname={$connection->dbname}", $connection->user, $connection->password);
+            $connection->connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $connection->connect;
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+}
