351e76a80a5e3b4a1dd00f6f9cd815638dfd36e0
CESCO <cescovalledev@gmail.com>
2022-10-10 01:11:48 -0600

    Lectura del CRUD hecha

--- app/assets/code.js
+++ app/assets/code.js
@@ -5,7 +5,20 @@ const app = new (function () {
     fetch("../controllers/listing.php")
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
+        this.tbody.innerHTML = "";
+        data.forEach((item) => {
+          this.tbody.innerHTML += `
+            <tr>
+                <td>${item.id}</td>
+                <td>${item.nombre}</td>
+                <td>${item.posicion}</td>
+                <td>${item.camiseta}</td>
+                <td>
+                <a href="javascript:void(0);" class="btn btn-warning btn-sm" onclick="app.edit(${item.id})">Editar</a>
+                <a href="javascript:void(0);" class="btn btn-danger btn-sm" onclick="app.delete(${item.id})">Eliminar</a>
+                </td>
+            `;
+        });
       })
       .catch((error) => {
         console.log(error);
